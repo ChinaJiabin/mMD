d44b79b6fbf71b1168200ee655c62e666e1f2c50
葛佳斌 <489509928@qq.com>
2014-01-12 20:49:07 +0800

    Update force.m

--- case/universalGravitation/force.m
+++ case/universalGravitation/force.m
@@ -1,7 +1,11 @@
 function f=force(vec_rel_pos)
-%Both F and vec_rel_pos are vector 
-%Universal gravitation
 
-len=norm(vec_rel_pos);
-f=len^-2*vec_rel_pos;
-   
+len=sqrt( sum(vec_rel_pos.^2,2) );
+id=len==0; len(id)=1;
+
+%--------------------------------
+fabs=len.^(-2); 
+%--------------------------------
+
+fabs(id)=0;
+f=repmat(fabs,1,3).*vec_rel_pos;
