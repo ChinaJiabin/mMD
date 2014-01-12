4f3f9ad086a00be563c2683e5afb81ae8137c31a
葛佳斌 <489509928@qq.com>
2014-01-12 20:51:20 +0800

    Update force.m

--- case/atom/force.m
+++ case/atom/force.m
@@ -1,6 +1,12 @@
 function f=force(vec_rel_pos)
-%Both F and vec_rel_pos are vector 
 
-len=norm(vec_rel_pos);
-f=48*(0.5*len^-8-len^-14)*vec_rel_pos;
-   
+len=sqrt( sum(vec_rel_pos.^2,2) );
+id=len==0; 
+len(id)=1;
+
+%-------------------------------------
+fabs=48*( 0.5*len.^(-8)-len.^(-14) );
+%-------------------------------------
+
+fabs(id)=0;
+f=repmat(fabs,1,3).*vec_rel_pos;
