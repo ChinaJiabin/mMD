b8f8466a5998a7fed8669cf65f0b9ce74d425da8
葛佳斌 <489509928@qq.com>
2014-01-12 20:53:24 +0800

    Update accel.m

--- src/accel.m
+++ src/accel.m
@@ -2,12 +2,9 @@
 %accelerated velocity
 vel_accel=zeros(N,3);
 
-for i=1:N-1
-    for j=i+1:N
-        vec_rel_pos=pos(j,:)-pos(i,:);
-        vel_accel(i,:)=vel_accel(i,:)+force(vec_rel_pos);
-        vel_accel(j,:)=vel_accel(j,:)+force(-vec_rel_pos);
-    end
+parfor i=1:N
+       vec_rel_pos=pos -repmat( pos(i,:),N,1 );
+       vel_accel=vel_accel+force(vec_rel_pos);
 end
 
 %--------------------------------------------------------------------------
